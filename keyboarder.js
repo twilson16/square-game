ef182cf142eaef335a8854d52a3eb1d56b5e9179
Tina <christinawilson6@gmail.com>
2018-08-20 08:50:26 -0400

    hopefully the foundations of movement, but no movement

--- keyboarder.js
+++ keyboarder.js
@@ -1,33 +1,33 @@
-class Keyboarder {
-  constructor() {
-    this.keyState = {}
+// class Keyboarder {
+//   constructor() {
+//     this.keyState = {}
 
-    window.addEventListener('keydown', function (e) {
-      this.keyState[e.keyCode] = true
-    }.bind(this))
+//     window.addEventListener('keydown', function (e) {
+//       this.keyState[e.keyCode] = true
+//     }.bind(this))
 
-    window.addEventListener('keyup', function (e) {
-      this.keyState[e.keyCode] = false
-    }.bind(this))
-  }
+//     window.addEventListener('keyup', function (e) {
+//       this.keyState[e.keyCode] = false
+//     }.bind(this))
+//   }
 
-  isDown(keyCode) {
-    return this.keyState[keyCode] === true
-  }
+//   isDown(keyCode) {
+//     return this.keyState[keyCode] === true
+//   }
 
-  on(keyCode, callback) {
-    window.addEventListener('keydown', function (e) {
-      if (e.keyCode === keyCode) {
-        callback()
-      }
-    })
-  }
-}
+//   on(keyCode, callback) {
+//     window.addEventListener('keydown', function (e) {
+//       if (e.keyCode === keyCode) {
+//         callback()
+//       }
+//     })
+//   }
+// }
 
-Keyboarder.KEYS = {
-  LEFT: 37,
-  RIGHT: 39,
-  UP: 38,
-  DOWN: 40,
-  S: 83
-}
+// Keyboarder.KEYS = {
+//   LEFT: 37,
+//   RIGHT: 39,
+//   UP: 38,
+//   DOWN: 40,
+//   S: 83
+// }
